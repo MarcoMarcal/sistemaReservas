06a8ea3e68f72dd88b41753e31fbc324b8f7fcf7
Marco <marquinho09.marcal@gmail.com>
2025-06-30 21:34:11 -0300

    feat: update controller

--- reservas/src/main/java/com/estudos/reservas/controller/TableController.java
+++ reservas/src/main/java/com/estudos/reservas/controller/TableController.java
@@ -1,18 +1,28 @@
 package com.estudos.reservas.controller;
 
-import com.estudos.reservas.persistence.Table;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import com.estudos.reservas.model.table.CreateTableRequest;
+import com.estudos.reservas.persistence.repository.TableRepository;
+import com.estudos.reservas.service.TableService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tables")
+@RequiredArgsConstructor
 public class TableController {
 
+    private final TableService tableService;
+    private final TableRepository tableRepository;
+
     @GetMapping()
-    public List<Table> tablesList() {
-        return null;
+    public ResponseEntity<?> tableList() {
+        return ResponseEntity.ok(tableRepository.findAll());
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> addTable(@RequestBody CreateTableRequest tableRequest) {
+        var response = tableService.createTable(tableRequest);
+        return ResponseEntity.ok(response);
     }
 }
